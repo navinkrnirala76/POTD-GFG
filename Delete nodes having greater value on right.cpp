73048a79e27f270f61b9341e9e338d0b1823ef0e
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-08-29 09:10:48 +0530

    Create Delete nodes having greater value on right.cpp

---
+++ Delete nodes having greater value on right.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        priority_queue<int> pq;
+        
+        return solve(head,pq);
+    }
+    Node* solve(Node *head,priority_queue<int> &pq)
+    {
+        if(head==nullptr)
+            return NULL;
+        head->next=solve(head->next,pq);
+        pq.push(head->data);
+        if(pq.top()>head->data)
+        {
+            Node *temp= head->next;
+            delete head;
+            return temp;
+        }
+        return head;
+    }
+    
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
