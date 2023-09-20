d1144e3425a47231f67bfe9a9affa4ab4d678022
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-09-21 02:25:28 +0530

    Create Stickler Thief.cpp

---
+++ Stickler Thief.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        vector<int>dp(n,0);
+        if(n==1)
+        {
+            return arr[0];
+        }
+        if(n==2)
+        {
+            return max(arr[0],arr[1]);
+        }
+        int maxi=0;
+        dp[0]=arr[0];
+        dp[1]=arr[1];
+        for(int i=2;i<n;i++)
+        {   maxi=max(maxi,dp[i-2]);
+            dp[i]=max(dp[i-2]+arr[i],maxi+arr[i]);
+        }
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            res=max(res,dp[i]);
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
