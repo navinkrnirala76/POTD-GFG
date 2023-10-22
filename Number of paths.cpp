70ce875fb9f9bc2564867f93bce13d9826989463
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-10-22 12:13:49 +0530

    Update Number of paths.cpp

--- Number of paths.cpp
+++ Number of paths.cpp
@@ -7,29 +7,41 @@ using namespace std;
 class Solution
 {
     public:
-    long long  numberOfPaths(int M, int N)
-    {
-        // Code Here
-          if (M == 0 || N == 0) {
-            return 0;
-        }
-        if(M == 1 || N == 1){
-            return 1;
-        }
+    const int MOD=1e9+7;
+    
+    long long modInverse(int a, int m) {
+    long long m0 = m, t, q;
+    long long x0 = 0, x1 = 1;
 
-        vector<long long> curr(N,0);
-        vector<long long> prev(N,1);
-    
-        // Fill the dp table iteratively
-        for (int i = M - 2; i >= 0; i--) {
-            curr[N-1] = 1;
-            for (int j = N - 2; j >= 0; j--) {
-                curr[j] = (prev[j] + curr[j+1]) % 1000000007;
-            }
-            prev = curr;
-        }
-    
-        return curr[0];
+    if (m == 1) return 0;
+
+    while (a > 1) {
+        q = a / m;
+        t = m;
+        m = a % m;
+        a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+
+    if (x1 < 0) x1 += m0;
+
+    return x1;
+}
+
+
+    long long  numberOfPaths(int m, int n)
+    {   if(m==1||n==1)
+            return 1;
+       long long ans = 1;
+        for (int i = m + n - 2, j = 1; i >= max(m, n); i--, j++) {
+        ans = (ans * i) % MOD;
+        ans = (ans * modInverse(j, MOD)) % MOD;
+    }
+
+    return ans;
+        
     }
 };
 
