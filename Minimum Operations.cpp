8ecd0089b57c0c990435e3de488e4674247f274e
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-10-26 15:43:37 +0530

    Create Minimum Operations.cpp

---
+++ Minimum Operations.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        if(n<=2) return n;
+        int curr=n,count=0;
+        while(curr>0){
+            if(curr%2==0) curr/=2;
+            else curr--;
+            count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
