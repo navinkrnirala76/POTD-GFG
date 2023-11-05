a9a74c3d1e18c0197128f957def49166d0e8abab
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-11-05 12:32:58 +0530

    Create Top K Frequent Elements in Array - |.cpp

---
+++ Top K Frequent Elements in Array - |.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> topK(vector<int>& nums, int k) {
+        // Code here
+        map<int,int>mp;
+        for(auto i : nums) mp[i]++;
+        
+        priority_queue<pair<int,int>>pq;
+        for(auto it : mp)
+        {
+            pq.push({it.second, it.first});
+        }
+        
+        vector<int>ans;
+        while(!pq.empty() and k--)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (auto &i : nums) cin >> i;
+        int k;
+        cin >> k;
+        Solution obj;
+        vector<int> ans = obj.topK(nums, k);
+        for (auto i : ans) cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
