900e208fad3bb0939d7f15b3eba74da9577f1ab8
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-11-14 14:17:49 +0530

    Create Check if strings are rotations of each other or not.cpp

---
+++ Check if strings are rotations of each other or not.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        int m = s1.size();
+        int n = s2.size();
+        if (m != n) return false;
+        s1 += s1;
+        return s1.find(s2) != string::npos;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
