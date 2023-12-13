32030dc2386136e25126035d7bf872a249604755
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2023-12-13 17:56:16 +0530

    Create Consecutive 1's not allowed.cpp

---
+++ Consecutive 1's not allowed.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	ll countStrings(int n) {
+	    // code here
+	     int a=1;
+	    int b=2;
+	    while(n>1)
+	    {
+	        int t=(a+b)%1000000007;
+	        a=b;
+	        b=t;
+	        n--;
+	    }
+	    return b;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
