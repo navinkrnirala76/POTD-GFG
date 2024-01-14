942c2ae563aefec7c5858cc854681518c5248af2
Navin Kumar <73814698+navinkrnirala76@users.noreply.github.com>
2024-01-14 13:40:39 +0530

    Create Find duplicate rows in a binary matrix.cpp

---
+++ Find duplicate rows in a binary matrix.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   
+    public:
+    vector<int> repeatedRows(vector<vector<int>> &matrix, int M, int N) 
+    { 
+        // Your code here
+       unordered_set<string>s;
+        vector<int>result;
+        for(int i=0;i<M;++i){
+            string str(matrix[i].begin(),matrix[i].end());
+            if(s.count(str)>0){
+                result.push_back(i);
+            }
+            else{
+                s.insert(str);
+            }
+            
+        }
+     return result; 
+    } 
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        vector<int> ans = ob.repeatedRows(matrix, row, col);
+
+
+        for (int i = 0; i < ans.size(); ++i)
+        {
+            cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
